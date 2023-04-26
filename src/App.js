ade49b4f4b63e7b5a89013d8699100b1728aceb0
NicolasRSJ <nicolassjesus@gmail.com>
2023-04-26 09:14:20 -0300

    Init Front-End

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="AppContainer">
+      <header>
+        <h1 className='AppContainer-header-title'>To-Do List</h1>
+      </header>
+      <article>
+        <h2 className='Appcontainer-article-description'>Essa é sua lista de tarefas online, para nunca se perde em seus compromissos e afazeres.</h2>
+      </article>
+      <section>
+        
+      </section>
+      <footer>
+        <span className="AppContainer-footer-span">2023 © To-Do List - Lista de Tarefas Online</span>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
